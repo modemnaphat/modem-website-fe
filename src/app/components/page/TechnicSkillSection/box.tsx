d5cee6b7cdbb23cbe056c7e3b839bfbedb46dc52
modemnaphat <119996385+modemnaphat@users.noreply.github.com>
2026-04-09 21:18:31 +0700

    update TechSkillSection

--- src/app/components/page/TechnicSkillSection/box.tsx
+++ src/app/components/page/TechnicSkillSection/box.tsx
@@ -14,18 +14,18 @@ export type Props = {
 const Box: React.FC<Props> = ({ data }) => {
   return (
     <div
-      className={`grid grid-cols-2 grid-rows-2 gap-8 w-full max-md:grid-cols-1 max-md:grid-rows-auto`}
+      className={`grid grid-cols-1 max-w-2xl mx-auto gap-8 w-full max-md:grid-cols-1`}
     >
       {data?.map((item, index) => (
-        <div key={index} className={`flex items-center gap-4`}>
+        <div key={index} className={`flex items-center gap-8 max-md:flex-col max-md:gap-4`}>
           <Image
             src={item.iconUrl ? item.iconUrl : "/modem-gry.svg"}
             alt={item.name + "icon"}
-            width={80}
-            height={80}
-            className="w-20 h-20 object-cover rounded-sm"
+            width={240}
+            height={135}
+            className="w-60 h-[135px] object-cover rounded-sm"
           />
-          <div className={`flex flex-col`}>
+          <div className={`flex flex-col max-md:text-center`}>
             <h5 className="text-2xl text-white font-bold">{item.name}</h5>
             <p className="text-md text-white">{item.description}</p>
           </div>
